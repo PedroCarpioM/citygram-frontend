ae6c6a39553f5da14ef5accc3c5b4e50be89f248
pedrocarpiom <pedrocarpiom@gmail.com>
2026-08-26 01:07:07 -0400

    feat(services): extend listings service with owner endpoints and hooks

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,6 +1,16 @@
 import axios, { type AxiosError } from 'axios'
 
+import { useAuthStore } from '~/store/auth'
+
 export const api = axios.create({ baseURL: import.meta.env.VITE_API_URL })
+
+api.interceptors.request.use((config) => {
+  const { accessToken } = useAuthStore.getState()
+  if (accessToken) {
+    config.headers.set('Authorization', `Bearer ${accessToken}`)
+  }
+  return config
+})
 
 api.interceptors.response.use(
   (response) => response,
